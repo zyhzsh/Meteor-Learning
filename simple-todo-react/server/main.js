3d6a21ce65dceaa9f4cc45fc03ba5bad89b41cae
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-11-30 18:56:41 +0100

    React+Meteor-To-Do-App

---
+++ simple-todo-react/server/main.js
@@ -0,0 +1,34 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+ 
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task'
+    ].forEach(insertTask)
+  }
+});
